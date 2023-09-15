6f13e0143cb4a830b0abefa9104da72e7af41182
Hanna Rosenfeld <hannarosenfeld@Hannas-Air.lan>
2023-09-15 12:48:16 -0400

    Style stage.

--- react-app/src/components/StagedVaults/index.js
+++ react-app/src/components/StagedVaults/index.js
@@ -1,35 +1,38 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { getAllStagedVaultsThunk } from "../../store/stage";
 
-import "./StagedVaults.css"
-
+import "./StagedVaults.css";
 
 export default function StagedVaults() {
-    const dispatch = useDispatch();
-    const staged = useSelector(state => state.stage.stagedVaults);
-    const stagedArr = Object.values(staged);
+  const dispatch = useDispatch();
+  const staged = useSelector((state) => state.stage.stagedVaults);
+  const stagedArr = Object.values(staged);
 
-    useEffect(() => {
-        dispatch(getAllStagedVaultsThunk())
-    }, [dispatch])
-    
-    useEffect(() => {
-        console.log("🪻", staged)
-    },[staged])
+  useEffect(() => {
+    dispatch(getAllStagedVaultsThunk());
+  }, [dispatch]);
 
-    return (
-        <div className="page-wrapper">
-            <div className="hazard-border">
-                <div className="staged-containers">
-                    {stagedArr?.map(vault => (
-                        <div key={vault.id} className="vault">
-                            <p>{vault?.customer?.name}</p>
-                            <p>{vault?.vault_id}</p>
-                        </div>
-                    ))}
-                </div>
+  // Function to truncate a string to a specified length
+  const truncateString = (str, maxLength) => {
+    if (str.length > maxLength) {
+      return str.slice(0, maxLength) + ".."; // Append ellipsis for truncated text
+    }
+    return str;
+  };
+
+  return (
+    <div className="page-wrapper">
+      <div className="hazard-border">
+        <div className="staged-containers">
+          {stagedArr?.map((vault) => (
+            <div key={vault.id} className="vault">
+              <p><b>{truncateString(vault?.customer?.name, 6)}</b></p>
+              <p>{vault?.vault_id}</p>
             </div>
+          ))}
         </div>
-    )
-}   
+      </div>
+    </div>
+  );
+}
