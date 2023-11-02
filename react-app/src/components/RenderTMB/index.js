f4184bbf448658979476f0745dd8d73c8f9a2807
Hanna Rosenfeld <hannarosenfeld@Hannas-Air.lan>
2023-11-02 13:06:01 -0400

    RenderTMB.

--- react-app/src/components/RenderTMB/index.js
+++ react-app/src/components/RenderTMB/index.js
@@ -7,57 +7,54 @@ import { getFieldThunk } from "../../store/field";
 
 const RenderTMB = ({ selectedField, top, middle, bottom, handleStageClick, handleOpenModal, handleEditClick }) => {
   const dispatch = useDispatch();
-  const fieldState = useSelector(state => state.field.currentField)
+  const fieldState = useSelector(state => state.field.currentField);
   const onlyBottom = !top && !middle && !bottom;
   const onlyMiddle = !top && !middle && bottom;
   const onlyTop = !top && middle && bottom;
 
-  console.log("🍰", selectedField)
+  console.log("🍰", selectedField);
 
   useEffect(() => {
     dispatch(getFieldThunk(selectedField?.id));
-  }, [dispatch])
-
-  // useEffect(() => {
-  //   console.log("🫖 fieldState", fieldState)
-  // }, [fieldState])
+  }, [dispatch]);
 
   return (
-    <>
-      <div className="selected-field-vaults-tmb">
-        <div className="top">
-          <span className="position">T</span>
-          {fieldState.full && <div style={{color: "red", display: "flex", alignItems: "center"}}><span class="material-symbols-outlined">warning</span>This field is full</div>}
-          {onlyTop && selectedField && !selectedField.full ? (
-            <AddVaultButton field={selectedField} position="T" handleOpenModal={handleOpenModal} />
-          ) : top ? (
-            <VaultInstance position="T" vault={top} handleStageClick={handleStageClick} handleEditClick={handleEditClick}/>
-          ) : (
-            ""
-          )}
-        </div>
-        <div className="middle">
-          <span className="position">M</span>
-          {onlyMiddle ? (
-            <AddVaultButton position="M" handleOpenModal={handleOpenModal}/>
-          ) : middle ? (
-            <VaultInstance position="M" vault={middle} handleStageClick={handleStageClick} handleEditClick={handleEditClick}/>
-          ) : (
-            ""
-          )}
-        </div>
-        <div className="bottom">
-          <span className="position">B</span>
-          {onlyBottom ? (
-            <AddVaultButton position="B" handleOpenModal={handleOpenModal}/>
-          ) : bottom ? (
-            <VaultInstance position="B" vault={bottom} handleStageClick={handleStageClick} handleEditClick={handleEditClick}/>
-          ) : (
-            ""
-          )}
-        </div>
+    <div className="selected-field-vaults-tmb">
+      <div className="top">
+        {fieldState.full ? (
+          <div style={{ color: "red", display: "flex", alignItems: "center" }}>
+            <span className="material-symbols-outlined">warning</span>
+            This field is full
+          </div>
+        ) : onlyTop && !fieldState.full ? (
+          <AddVaultButton
+            field={selectedField}
+            position="T"
+            handleOpenModal={handleOpenModal}
+          />
+        ) : top ? (
+          <VaultInstance
+            position="T"
+            vault={top}
+            handleStageClick={handleStageClick}
+            handleEditClick={handleEditClick}
+          />
+        ) : null}
       </div>
-    </>
+      <div className="bottom">
+        <span className="position">B</span>
+        {onlyBottom ? (
+          <AddVaultButton position="B" handleOpenModal={handleOpenModal} />
+        ) : bottom ? (
+          <VaultInstance
+            position="B"
+            vault={bottom}
+            handleStageClick={handleStageClick}
+            handleEditClick={handleEditClick}
+          />
+        ) : null}
+      </div>
+    </div>
   );
 };
 
