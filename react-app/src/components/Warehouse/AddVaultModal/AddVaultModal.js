b7d2a01b2b8ce8a15f1d20c8c926410da4343768
Hanna Rosenfeld <hannarosenfeld@Hannas-Air.lan>
2023-11-13 14:41:34 -0500

    Create seperate utility file for helper function.

--- react-app/src/components/Warehouse/AddVaultModal/AddVaultModal.js
+++ react-app/src/components/Warehouse/AddVaultModal/AddVaultModal.js
@@ -1,7 +1,6 @@
 import { useState, useEffect  } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { addVaultToWarehouseThunk, getAllWarehouseVaultsThunk, getWarehouseInfoThunk} from '../../../store/warehouse';
+import { getWarehouseInfoThunk} from '../../../store/warehouse';
 import { getAllCustomersThunk, addCustomerThunk } from '../../../store/customer'
 import { addVaultThunk, getAllVaultsThunk, getVaultThunk } from '../../../store/vault';
 import Paper from '@mui/material/Paper';
@@ -13,7 +12,6 @@ import "./AddVaultModal.css"
 import MiniWareHouse from './MiniWareHouse';
 import { getAllFieldsThunk, getFieldThunk } from '../../../store/field';
 import { addOrderThunk } from '../../../store/order';
-
 
 
 export default function AddVaultModal({ onClose, selectedField, tmb, updateSelectedFieldVaults}) {
@@ -166,14 +164,12 @@ export default function AddVaultModal({ onClose, selectedField, tmb, updateSelec
 
         const newVault = await dispatch(addVaultThunk(vaultData));
 
-        // const updatedVault = await dispatch(addVaultToWarehouseThunk(newVault.id));
-
         // Ensure that addVaultToWarehouseThunk returns the updated vault
-          // if (updatedVault) {
-          //   const updateSelectedFieldVaultsThing = await updateSelectedFieldVaults(updatedVault);
-          // } else {
-          //   console.error('updatedVault is null or undefined');
-          // }
+          if (newVault) {
+            const updateSelectedFieldVaultsThing = await updateSelectedFieldVaults(newVault);
+          } else {
+            console.error('updatedVault is null or undefined');
+          }
       
         // Step 4: Fetch other data (if needed)
         const getWarehouseInfoDispatch = await dispatch(getWarehouseInfoThunk());
---
+++ react-app/src/components/utility.js
@@ -0,0 +1,31 @@
+
+export function rowCreator(fields) {
+    const RowArray = [];
+
+    for (let letter of "ABCDEFGHI") {
+        RowArray.push({ fields: [], id: letter })
+    }
+
+    fields.map(field => {
+        for (let row of RowArray) {
+            if (row.id === field.row_id) {
+                row.fields.push(field)
+            }
+        }
+    })
+
+    return RowArray;
+}
+
+export const sortFields = (fields) => {
+    const sortedFields = {};
+    let sortedFieldsArr;
+
+    for (let field of fields) {
+        sortedFields[field.id] = field
+    }
+
+    sortedFieldsArr = Object.values(sortedFields)
+
+    return sortedFieldsArr
+}
