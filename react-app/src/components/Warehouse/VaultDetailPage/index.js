5243efa44a2f5d6f5c7837bcd1b7543f2507b7f6
Hanna Rosenfeld <hannarosenfeld@Hannas-Air.lan>
2023-12-13 11:08:10 -0500

    Vault detail view.

--- react-app/src/components/Warehouse/VaultDetailPage/index.js
+++ react-app/src/components/Warehouse/VaultDetailPage/index.js
@@ -21,60 +21,56 @@ const VaultDetailPage = () => {
     }, [vaultId]);
 
     useEffect(() => {
-        if (vaultObj.customer && vaultObj.customer.id) {
-            dispatch(getCustomerThunk(vaultObj.customer.id));
-        }
-    }, [vaultObj]);
+        if (attachments) setSelectedPDF(attachments[0].file_url)
+    }, [attachments])
 
     const handleAttachmentClick = (fileUrl) => {
-        setSelectedPDF(fileUrl)
+        setSelectedPDF(fileUrl);
         console.log('Attachment Clicked:', selectedPDF);
     };
 
     return (
         <>
             {vaultObj && vaultObj.customer && customerObj.vaults ? (
-                <div className="container mt-3" style={{ display: 'flex', maxWidth: '800px', height: '85vh' }}>
-                    <div className="card" style={{ flex: 1, marginRight: '20px' }}>
+                <div className="container mt-3">
+                    <div className="card" style={{ marginBottom: '20px' }}>
                         <div className="card-body">
                             <h2 className="card-title">Vault Detail View</h2>
-                            <div className="row">
-                                <div className="col">
-                                    <strong>Vault</strong> #{vaultObj.vault_id}
-                                </div>
-                                <div className="col">
-                                    <strong>Order</strong> #{vaultObj.order_number}
-                                </div>
-                                <div className="col">
-                                    <strong>Customer</strong> {vaultObj.customer.name}
-                                </div>
-                                <div className="col">
-                                    <strong>Customer Vaults:</strong> 10
-                                </div>
+                            <div>
+                                <strong>Vault</strong> #{vaultObj.vault_id}
                             </div>
-                            <div className="row mt-3">
-                                <div className="col d-flex flex-column">
-                                    <strong className="mb-2">Attachments</strong>
-                                    {attachments.map((attachment) => (
-                                        <div
-                                            className="d-flex"
-                                            key={attachment.id}
-                                            onClick={() => handleAttachmentClick(attachment.file_url)}
-                                        >
-                                            <span className="material-symbols-outlined" style={{ fontSize: '1.5em' }}>
-                                                file_present
-                                            </span>
-                                            <small>{attachment.file_name}</small>
-                                        </div>
-                                    ))}
-                                </div>
+                            <div>
+                                <strong>Order</strong> #{vaultObj.order_number}
+                            </div>
+                            <div>
+                                <strong>Customer</strong> {vaultObj.customer.name}
+                            </div>
+                            <div>
+                                <strong>Customer Vaults:</strong> 10
                             </div>
                         </div>
                     </div>
-                    <div style={{ border: '2px solid grey', height: '85vh', width: '60%', overflow: 'auto' }}>
-                        { selectedPDF && (
-                            <embed src={selectedPDF} width="100%" height="100%"/>                            
-                        )}                                        
+                    <div className="card">
+                        <div className="card-body">
+                            <strong className="mb-3">Attachments</strong>
+                            {attachments.map((attachment) => (
+                                <div
+                                    className="d-flex"
+                                    key={attachment.id}
+                                    onClick={() => handleAttachmentClick(attachment.file_url)}
+                                >
+                                    <span className="material-symbols-outlined" style={{ fontSize: '1.5em' }}>
+                                        file_present
+                                    </span>
+                                    <small>{attachment.file_name}</small>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="card mt-3">
+                        <div className="card-body">
+                            {selectedPDF && <embed src={selectedPDF} width="100%" height="500" />}
+                        </div>
                     </div>
                 </div>
             ) : (
