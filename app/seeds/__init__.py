781bd0f4742e907b9301b9144b2e9b7fd62a2ea4
Hanna Rosenfeld <hannarosenfeld@Hannas-Air.lan>
2023-12-25 09:59:19 -0500

    Conditionally seed.

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -5,8 +5,9 @@ from .customers import seed_customers, undo_customers
 from .fields import seed_fields, undo_fields
 from .rows import seed_rows, undo_rows
 from .warehouse import seed_warehouse, undo_warehouse
-from .stage import seed_stage, undo_stage
+from .stage import seed_stage, undo_stage 
 from .orders import seed_orders, undo_orders
+from app.models import Row, Field, Warehouse, Stage, Customer, User, Order, Vault
 
 from app.models.db import db, environment, SCHEMA
 
@@ -22,29 +23,41 @@ def seed():
         # command, which will truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_stage() 
-        undo_vaults()
-        undo_fields()
-        undo_rows()
-        undo_orders()
-        undo_customers()
-        undo_users()
-        undo_warehouse()
+        # undo_stage()
+        # undo_vaults()
+        # undo_fields()
+        # undo_rows()
+        # undo_orders()
+        # undo_customers()
+        # undo_users()
+        # undo_warehouse()
         # when ready to go live, add conditional that checks of those rows exist, plus remove undo_.. commands
-    seed_users()
-    seed_customers()
-    seed_orders()
-    seed_rows()
-    seed_fields()
-    seed_vaults()
-    seed_warehouse()
-    seed_stage()
+
+        rows = Row.query.all()
+        fields = Field.query.all()
+        warehouse = Warehouse.query.all()
+        stage = Stage.query.all()
+        customers = Customer.query.all()
+        users = User.query.all()
+        orders = Order.query.all()
+        vaults = Vault.query.all()
+
+        print("💖")
+        if not users: seed_users()
+        if not customers : seed_customers()
+        if not orders: seed_orders()
+        if not rows: seed_rows() 
+        if not fields: seed_fields() 
+        if not vaults: seed_vaults()
+        if not warehouse: seed_warehouse()
+        if not stage: seed_stage()
 
 @seed_commands.command('undo')
 def undo():
-    undo_stage() 
+    undo_stage()  # Undo the Stage model first
     undo_vaults()
     undo_fields()
     undo_rows()
     undo_customers()
     undo_users()
+    # Add other undo functions here
