94d7ae52ea5f400875871b9eb74b4c0d456db979
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2024-02-08 11:37:31 -0500

    .

--- app/seeds/fields.py
+++ app/seeds/fields.py
@@ -5,15 +5,15 @@ def seed_fields():
     fields = []
 
     # Create fields for each row
-    for row_num in range(1, 10):
-        row_char = str(row_num)
+    for row_char in 'ABCDEFGHI':
         for field_num in range(1, 13):
-            field_id = f"{row_char}{field_num}"
+            field_id = f"{row_char}{field_num:02d}"  # Ensure field_num has at least two digits
             field = Field(row_id=row_char, field_id=field_id)
             fields.append(field)
 
     db.session.add_all(fields)
     db.session.commit()
+
 
 def undo_fields():
     if environment == "production":
