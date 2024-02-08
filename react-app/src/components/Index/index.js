a494ace05e05dfbeef4d106ff9179befca9a00f9
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2024-02-08 09:23:28 -0500

    Frontend: Update Index.

--- react-app/src/components/Index/index.js
+++ react-app/src/components/Index/index.js
@@ -3,7 +3,8 @@ import Warehouse from "../Warehouse";
 export default function Index() {
     return(
         <div className="wrapper">
-            <Warehouse />
+            we moved things around. please access warehouse from the side drawer.
+            {/* <Warehouse /> */}
         </div>
     )
 }
