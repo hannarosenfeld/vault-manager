feb79f81f18a3383f155a59b798a12cf46a53ba2
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2024-02-15 18:22:27 -0500

    Add Index Navigation.

--- react-app/src/components/Index/index.js
+++ react-app/src/components/Index/index.js
@@ -1,10 +1,35 @@
 import Warehouse from "../Warehouse";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import { getAllWarehousesThunk } from "../../store/warehouse";
+import { useEffect } from "react";
+
 
 export default function Index() {
+    const dispatch = useDispatch();
+    const warehouses = useSelector(state => state.warehouse.warehouses);
+
+    useEffect(() => {
+        dispatch(getAllWarehousesThunk());
+    }, [])
+
     return(
-        <div className="wrapper">
+        <div className="wrapper" style={{marginTop: "1em"}}>
             {/* <Warehouse warehouseId="1"/> */}
-            please select a warehouse from the drawer
+            <div style={{display: "flex", gap: "1em"}}>
+            {warehouses.map(warehouse => (
+                <div className="card" style={{width: "18rem"}}>
+                    {/* <img src="..." class="card-img-top" alt="..." /> */}
+                    <div class="card-body">
+                        <NavLink to={`/warehouse/${warehouse.id}`}>
+                        <h5 class="card-title">{warehouse.name}</h5>
+                        </NavLink>
+                        {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a> */}
+                    </div>
+                </div>
+            ))}
+            </div>
         </div>
     )
 }
