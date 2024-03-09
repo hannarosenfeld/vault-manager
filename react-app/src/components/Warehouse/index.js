6beae86bd25ea2b4732280e229e8f7eb8f25ea58
hannarosenfeld <hannazrosenfeld@gmail.com>
2024-03-08 21:31:20 -0500

    Styling: give fields a max-height and add a negative margin bottom since that seems necessery.

--- react-app/src/components/Warehouse/index.js
+++ react-app/src/components/Warehouse/index.js
@@ -111,25 +111,24 @@ export default function Warehouse() {
             for (let i = 0; i < warehouse.rows; i++) {
                 for (let j = 0; j < warehouse.rows; j++) {
                     let field = temp[j*warehouse.rows+i]
-                    field && res.push(<div className="field"
-                                key={field.id}
-                                style={{
-                                    backgroundColor: `${
-                                        field.vaults.length === 3 || field.full ? "var(--red)" :
-                                            field.vaults.length === 2 ? "var(--yellow)" :
-                                                field.vaults.length === 1 ? "var(--green)" :
-                                                    "var(--lightgrey)"
-                                    }`,
-                                    border: `${selectedFieldId === field.id ? "3px solid var(--blue)" : "none"}`,
-                                    marginBottom: `${field.type === "couchbox-T" ? "-2.2em" : ''}`,
-                                    width: `${field.type === "couchbox-B" ? "0px" : ''}`,
-                                    zIndex: `${field.type === "couchbox-B" ? "100" : 'none'}`,
-                                }}
-                                onClick={() => handleFieldClick(field.id)}
-                            >
-                                {field.type === "couchbox-B" ? "" : <div className="field-number">{field.name}</div>}
-
-                            </div>)
+                    field && res.push(
+                        <div className="field"
+                            key={field.id}
+                            style={{
+                                backgroundColor: `${
+                                    field.vaults.length === 3 || field.full ? "var(--red)" :
+                                        field.vaults.length === 2 ? "var(--yellow)" :
+                                            field.vaults.length === 1 ? "var(--green)" :
+                                                "var(--lightgrey)"
+                                }`,
+                                border: `${selectedFieldId === field.id ? "3px solid var(--blue)" : "none"}`,
+                                marginBottom: `${field.type === "couchbox-T" ? "-2.2em" : '-2.2em'}`,
+                                width: `${field.type === "couchbox-B" ? "0px" : ''}`,
+                                zIndex: `${field.type === "couchbox-B" ? "100" : 'none'}`,
+                            }}
+                            onClick={() => handleFieldClick(field.id)}
+                        >{field.type === "couchbox-B" ? "" : <div className="field-number">{field.name}</div>}</div>
+                    )
                 }
         }
         return res.map((el) => <>{el}</>)
