a27a87a6aefbb87582e0dea3b0d17b3f06c4a521
hannarosenfeld <hannazrosenfeld@gmail.com>
2024-03-27 20:33:59 -0400

    Change height of coucbox.

--- react-app/src/components/Warehouse/index.js
+++ react-app/src/components/Warehouse/index.js
@@ -134,7 +134,7 @@ export default function Warehouse() {
                                 searchResult && searchResult?.includes(field.id) ? "3px solid var(--blue)" :
                                 "none"
                             }`,
-                            marginBottom: `${field.type === "couchbox-T" ? "-2.2em" : '0'}`,
+                            marginBottom: `${field.type === "couchbox-T" ? "-2.6em" : '0'}`,
                             width: `${field.type === "couchbox-B" ? "0px" : ''}`,
                             zIndex: `${field.type === "couchbox-B" ? "100" : 'none'}`,
                         }}
