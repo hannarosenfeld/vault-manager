c6a35f09b01cd965161f99000082c05657a6f1ab
hannarosenfeld <hannazrosenfeld@gmail.com>
2024-04-15 13:39:29 -0400

    Edit Warehouse Modal text.

--- react-app/src/components/EditWarehousePage/editWarehouseModal.js
+++ react-app/src/components/EditWarehousePage/editWarehouseModal.js
@@ -6,7 +6,7 @@ export function EditWarehouseModal({ dir, opperation }) {
 
   return (
     <div style={{width: "22em", padding: "3em"}}>
-      {opperation === "add" ? <div>Add <b>{count}</b> row{count === 1 ? '' : 's' } to warehouse</div> : <div>Subtract rows from warehouse</div>}
+      {opperation === "add" ? <div>Add <b>{count}</b> row{count === 1 ? '' : 's' } to warehouse</div> : <div>Subtract <b>{count}</b> row{count === 1 ? '' : 's' } from warehouse</div>}
       <div className="input-group">
         <button
           className="btn btn-outline-secondary"
