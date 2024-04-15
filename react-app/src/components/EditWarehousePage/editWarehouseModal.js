10cb1a97f7861198bde330a8778e7cea5860d17a
koreanpro22 <dhskim22@gmail.com>
2024-04-15 12:14:17 -0400

    add counter on modal

--- react-app/src/components/EditWarehousePage/editWarehouseModal.js
+++ react-app/src/components/EditWarehousePage/editWarehouseModal.js
@@ -1,14 +1,17 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export function EditWarehouseModal({dir, opperation}) {
-  const [show, setShow] = useState(true);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [count, setCount] = useState(1);
 
   return (
     <>
-      {dir} {opperation}
+      {opperation === "add" ? <div>Add rows from warehouse</div> : <div>Subtract rows from warehouse</div>}
+      <div>
+      {(opperation === "add" && count === 1) ? "" : <i class="fa-solid fa-arrow-left" onClick={() => setCount(count-1)}></i>}
+      {count}
+      <i class="fa-solid fa-arrow-right" onClick={() => setCount(count+1)}></i>
+      </div>
     </>
   );
 }
