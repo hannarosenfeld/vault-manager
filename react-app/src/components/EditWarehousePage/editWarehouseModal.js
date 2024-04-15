d548100d53f47a3af463039fc9b8abb3794f6faf
hannarosenfeld <hannazrosenfeld@gmail.com>
2024-04-15 13:16:06 -0400

    Update EditWarehouseModal.

--- react-app/src/components/EditWarehousePage/editWarehouseModal.js
+++ react-app/src/components/EditWarehousePage/editWarehouseModal.js
@@ -1,17 +1,28 @@
 import { useState } from "react";
 
-export function EditWarehouseModal({dir, opperation}) {
+export function EditWarehouseModal({ dir, opperation }) {
 
   const [count, setCount] = useState(1);
 
   return (
-    <>
-      {opperation === "add" ? <div>Add rows from warehouse</div> : <div>Subtract rows from warehouse</div>}
-      <div>
-      {(opperation === "add" && count === 1) ? "" : <i class="fa-solid fa-arrow-left" onClick={() => setCount(count-1)}></i>}
-      {count}
-      <i class="fa-solid fa-arrow-right" onClick={() => setCount(count+1)}></i>
+    <div style={{width: "22em", padding: "3em"}}>
+      {opperation === "add" ? <div>Add <b>{count}</b> row{count === 1 ? '' : 's' } to warehouse</div> : <div>Subtract rows from warehouse</div>}
+      <div className="input-group">
+        <button
+          className="btn btn-outline-secondary"
+          disabled={count === 1}
+          onClick={() => setCount(count - 1)}
+        >
+          -
+        </button>
+        {/* <input type="text" className="form-control text-center" value={count} readOnly /> */}
+        <button
+          className="btn btn-outline-secondary"
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
       </div>
-    </>
+    </div>
   );
 }
