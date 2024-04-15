1a9652e63c5c5a35e6aff5515e1cae9ff2997eb6
hannarosenfeld <hannazrosenfeld@gmail.com>
2024-04-15 09:45:30 -0400

    Add Link to edit warehouse page.

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -75,12 +75,14 @@ function ProfileButton({ user }) {
               to={`/${warehouse.companyName.toLowerCase()}/warehouse/${warehouse.id}`}
               style={{display: "flex", gap: "5px"}}>
               <ListItemText primary={warehouse.name} />
-              <span
-            style={{ color: '#0074D9' }}
-            className="material-symbols-outlined"
-          >
-            edit
-          </span>
+              <NavLink to={`/warehouse/${warehouse.id}/edit`}>
+                <span
+                  style={{ color: '#0074D9'}}
+                  className="material-symbols-outlined"
+                >
+                edit
+              </span>
+            </NavLink>
             </ListItem>  
               ))    
 
