4f2e094354a6944aa7521250adfc730595f56e76
hannarosenfeld <hannazrosenfeld@gmail.com>
2024-05-07 21:45:44 -0400

    .

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,8 @@ from flask import Flask, render_template, request, session, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
-from flask_login import LoginManager
-
+from flask_login import LoginManager, login_required
 from .models import db, User
-
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.vault_routes import vault_routes
@@ -84,6 +82,7 @@ def inject_csrf_token(response):
 
 
 @app.route("/api/docs")
+@login_required
 def api_help():
     """
     Returns all API routes and their doc strings
