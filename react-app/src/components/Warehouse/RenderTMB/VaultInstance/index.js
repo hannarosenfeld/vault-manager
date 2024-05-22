0367a5ccac0fbca945f4c7f2751795e0a2f597c5
hannarosenfeld <hannazrosenfeld@gmail.com>
2024-05-22 13:38:36 -0400

    EMPTY when null.

--- react-app/src/components/Warehouse/RenderTMB/VaultInstance/index.js
+++ react-app/src/components/Warehouse/RenderTMB/VaultInstance/index.js
@@ -12,7 +12,7 @@ const VaultInstance = ({ topmostVault, vault, handleStageClick, fieldType }) => 
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
       <div style={{ display: 'flex', width: '60%', gap: '5px' }}>
-        <div>{vault.customer_name === null ? "EMPTY" : vault.customer_name}</div>
+        <div>{vault.customer_name === null || "null" ? "EMPTY" : vault.customer_name}</div>
         { fieldType !== "couchbox-T" ? (
           <div style={{display: 'flex', gap: '0.5em'}}>
             <div>{vault.name}</div>
