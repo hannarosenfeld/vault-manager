9ceb1ec116d852230c322b463f2a67678083538e
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-01-22 11:34:27 -0500

    Improve styling of move vault into warehouse modal.

--- react-app/src/components/Stage/StageToWareHouseModal/StageToWareHouseModal.js
+++ react-app/src/components/Stage/StageToWareHouseModal/StageToWareHouseModal.js
@@ -2,13 +2,11 @@ import { useEffect, useState } from "react";
 import { IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllWarehousesThunk  } from "../../../store/warehouse";
+import { getAllWarehousesThunk } from "../../../store/warehouse";
 import { moveVaultFromStageToWarehouseThunk, getVaultsThunk } from "../../../store/vault";
 import MiniWareHouse from "./MiniWareHouse";
 import RenderTMB from "../../Warehouse/RenderTMB";
-import "./StageToWareHouseModal.css";
 import { setSelectedFieldAction } from "../../../store/field";
-
 
 export default function StageToWareHouseModal({ closeModal, selectedVault }) {
   const dispatch = useDispatch();
@@ -16,71 +14,111 @@ export default function StageToWareHouseModal({ closeModal, selectedVault }) {
   const warehousesObj = useSelector((state) => state.warehouse);
   const warehouses = Object.values(warehousesObj);
   const warehouse = useSelector((state) => state.warehouse[selectedWarehouse]);
-  const selectedField = useSelector((state )=> state.field.selectedField);
-  // const [loadingVaults, setLoadingVaults] = useState(false);
-
+  const selectedField = useSelector((state) => state.field.selectedField);
 
   useEffect(() => {
-    // dispatch(getAllWarehousesThunk());
     dispatch(setSelectedFieldAction(null));
-    // setLoadingVaults(true);
   }, [selectedWarehouse]);
 
-
   const moveVault = async (vault, position) => {
-    // if (loadingVaults) {
-    //   return;
-    // }
     if (selectedField) {
-      await dispatch(moveVaultFromStageToWarehouseThunk(vault.id, selectedField.id, position))
-      await dispatch(getVaultsThunk())
-     closeModal();
+      await dispatch(moveVaultFromStageToWarehouseThunk(vault.id, selectedField.id, position));
+      await dispatch(getVaultsThunk());
+      closeModal();
     }
   };
-  
+
+  const styles = {
+    modalWrapper: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100vw",
+      height: "100vh",
+      background: "rgba(0, 0, 0, 0.5)",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: "0.3em",
+    },
+    modalContent: {
+      background: "#fff",
+      padding: "1em",
+      boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.3)",
+      width: "100%",
+      maxWidth: "800px",
+      height: "100%",
+      overflow: "scroll",
+    },
+    vaultInfo: {
+      width: "90%",
+      display: "flex",
+      gap: "1em"
+    },
+    warehousesButtonContainer: {
+      display: "flex",
+      flexWrap: "wrap",
+      gap: "0.5em",
+    },
+    fieldInfo: {
+      height: "9em",
+    },
+    closeButton: {
+      position: "absolute",
+      top: "0",
+      right: "0",
+      zIndex: 1,
+    },
+  };
+
   return (
-    <div className="stage-to-warehouse-modal-wrapper">
-      <div className="stage-to-warehouse-modal-content">
-        <div className="modal-content-wrapper">
-          <div className="modal-close-button">
-            <IconButton
-              aria-label="Close"
-              onClick={closeModal}
-            >
+    <div style={styles.modalWrapper}>
+      <div style={styles.modalContent}>
+        <div style={{ position: "relative" }}>
+          <div style={styles.closeButton}>
+            <IconButton aria-label="Close" onClick={closeModal}>
               <CloseIcon />
             </IconButton>
           </div>
           {selectedVault && (
             <div>
-              <div style={{display: "flex"}}>
-              <div className="vault-info">
-                <h4>Selected Vault Information</h4>
-                <p><b>Customer:</b> {selectedVault.customer_name}</p>
-                <p><b>Vault ID:</b> {selectedVault.name}</p>
-              </div>
-              <div className="warehouses-button-container">
-                {warehouses.map(warehouse => (
-                    <button onClick={() => setSelectedWarehouse(warehouse.id)} type="button" className={`${warehouse.id === selectedWarehouse ? 'btn btn-primary' : 'btn btn-secondary'}`}>{warehouse.name}</button>
-                ))}
-              </div>
+              <div style={{ display: "flex", flexDirection: "column", gap: "0.5em"}}>
+              <h4 style={{marginTop: "1em"}}>Move Vault into Warehouse</h4>
+              <div style={styles.warehousesButtonContainer}>
+                  {warehouses.map((warehouse) => (
+                    <button
+                      style={{margin: "0", }}
+                      key={warehouse.id}
+                      onClick={() => setSelectedWarehouse(warehouse.id)}
+                      type="button"
+                      className={`${
+                        warehouse.id === selectedWarehouse ? "btn btn-primary" : "btn btn-secondary"
+                      }`}
+                    >
+                      {warehouse.name}
+                    </button>
+                  ))}
+                </div>
+                <div style={styles.vaultInfo}>
+                  <div><b>Customer:</b> {selectedVault.customer_name}</div>
+                  <div><b>Vault ID:</b> {selectedVault.name}</div>
+                </div>
               </div>
               <div>
-                <div className="field-info">
+                <div style={styles.fieldInfo}>
                   {selectedField ? (
-                    <RenderTMB 
+                    <RenderTMB
                       selectedFieldId={selectedField.id}
                       selectedVault={selectedVault}
                       moveVault={moveVault}
                       warehouse={warehouse}
-                      component={'stage'}
+                      component={"stage"}
                     />
                   ) : (
-                    <div style={{height: "9em"}}>
-                      Select a field to view its info
-                    </div>
+                    <div style={styles.fieldInfo}>Select a field to view its info</div>
                   )}
                 </div>
-                  <MiniWareHouse warehouseId={selectedWarehouse}/>                
+                <MiniWareHouse warehouseId={selectedWarehouse} />
               </div>
             </div>
           )}
