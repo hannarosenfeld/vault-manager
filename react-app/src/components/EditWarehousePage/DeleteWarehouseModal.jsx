832cf8bb975679c21b0c85fed660f430f97f561e
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-01-27 12:44:53 -0500

    Simplify EditWarehouse component.

--- react-app/src/components/EditWarehousePage/DeleteWarehouseModal.jsx
+++ react-app/src/components/EditWarehousePage/DeleteWarehouseModal.jsx
@@ -1,22 +1,25 @@
-import { Modal } from "react-bootstrap";
-
-export default function DeleteWarehouseModal({ show, onClose, onConfirm }) {
+const DeleteWarehouseModal = ({ closeModal, confirmDelete }) => {
   return (
-    <Modal show={show} onHide={onClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Delete Warehouse</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p>Are you sure you want to delete this warehouse?</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <button onClick={onConfirm} className="btn btn-danger">
-          Yes
-        </button>
-        <button onClick={onClose} className="btn btn-secondary">
-          No
-        </button>
-      </Modal.Footer>
-    </Modal>
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10">
+      <div className="bg-white p-5 rounded-lg shadow-lg text-center">
+        <p>Are you sure you want to delete the warehouse?</p>
+        <div className="mt-5">
+          <button
+            onClick={confirmDelete}
+            className="px-5 py-2 bg-red-500 text-white rounded-md mr-2 cursor-pointer"
+          >
+            Yes
+          </button>
+          <button
+            onClick={closeModal}
+            className="px-5 py-2 bg-gray-300 rounded-md cursor-pointer"
+          >
+            No
+          </button>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default DeleteWarehouseModal;
--- react-app/src/components/EditWarehousePage/index.jsx
+++ react-app/src/components/EditWarehousePage/index.jsx
@@ -7,120 +7,135 @@ import { EditWarehouseModal } from "./editWarehouseModal";
 import OpenModalButton from "../OpenModalButton";
 import fieldGenerator from "./fieldGenerator";
 import { sortFields } from "../utility";
+import DeleteWarehouseModal from "./DeleteWarehouseModal";
 
 export default function EditWarehousePage() {
-    const dispatch = useDispatch();
-    const { warehouseId } = useParams();
-    const navigate = useNavigate();
-    const warehouse = useSelector(state => state.warehouse[warehouseId])
-    const allFields = useSelector((state) => state.field[warehouseId]);
-    const [loadedWarehouseFields, setLoadedWarehouseFields] = useState(false);
-    const [fields, setFields] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const dispatch = useDispatch();
+  const { warehouseId } = useParams();
+  const navigate = useNavigate();
+  const warehouse = useSelector((state) => state.warehouse[warehouseId]);
+  const allFields = useSelector((state) => state.field[warehouseId]);
+  const [loadedWarehouseFields, setLoadedWarehouseFields] = useState(false);
+  const [fields, setFields] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const ModalButton = ({ dir, operation, warehouseId }) => (
-        <OpenModalButton
-            buttonText={<span className="material-symbols-outlined">{operation === 'add' ? 'add' : 'remove'}</span>}
-            onItemClick={() => setIsModalOpen(false)}
-            modalComponent={<EditWarehouseModal dir={dir} opperation={operation} warehouseId={warehouseId} />}
+  const ModalButton = ({ dir, operation, warehouseId }) => (
+    <OpenModalButton
+      buttonText={
+        <span className="material-symbols-outlined">
+          {operation === "add" ? "add" : "remove"}
+        </span>
+      }
+      onItemClick={() => setIsModalOpen(false)}
+      modalComponent={
+        <EditWarehouseModal
+          dir={dir}
+          opperation={operation}
+          warehouseId={warehouseId}
         />
-    );
-    
-    // Delete Warehouse Modal
-    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-    const openDeleteModal = () => setIsDeleteModalOpen(true);
-    const closeDeleteModal = () => setIsDeleteModalOpen(false);
-    const confirmDelete = async () => {
-        setIsDeleteModalOpen(false);
-        await dispatch(deleteWarehouseThunk(warehouseId));
-        navigate("/");
-    };
+      }
+    />
+  );
 
-    useEffect(() => {
-        setFields(null);
-        setLoadedWarehouseFields(false);
-        const fields = dispatch(getAllFieldsThunk(warehouseId));
+  // Delete Warehouse Modal
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const openDeleteModal = () => setIsDeleteModalOpen(true);
+  const closeDeleteModal = () => setIsDeleteModalOpen(false);
+  const confirmDelete = async () => {
+    setIsDeleteModalOpen(false);
+    await dispatch(deleteWarehouseThunk(warehouseId));
+    navigate("/");
+  };
 
-        Promise.all([fields])
-            .then(() => setLoadedWarehouseFields(true))
-            .catch(() => console.log("🚨 fields could not be loaded!"));
-    }, [dispatch, warehouseId]);
+  useEffect(() => {
+    setFields(null);
+    setLoadedWarehouseFields(false);
+    const fields = dispatch(getAllFieldsThunk(warehouseId));
 
-    useEffect(() => {
-        if (loadedWarehouseFields && allFields) {
-            const fieldsArr = Object.values(allFields);
-            if (fieldsArr.length) {
-                const sortedFields = sortFields(fieldsArr);
-                setFields(sortedFields);
-            }
-        }
-    }, [loadedWarehouseFields, allFields]);
+    Promise.all([fields])
+      .then(() => setLoadedWarehouseFields(true))
+      .catch(() => console.log("🚨 fields could not be loaded!"));
+  }, [dispatch, warehouseId]);
 
-    return (
-        <div>
-            <div className="flex flex-col items-center h-full">
-                <div className="wrapper flex flex-col items-center w-full h-full">
-                    <div className="flex w-full mt-4 relative">
-                        {/* Left Buttons */}
-                        <div className="leftButtons absolute left-0 top-0 flex flex-col items-center mx-2">
-                            <ModalButton dir="left" operation="add" warehouseId={warehouseId} />
-                            <ModalButton dir="left" operation="subtract" warehouseId={warehouseId} />
-                        </div>
+  useEffect(() => {
+    if (loadedWarehouseFields && allFields) {
+      const fieldsArr = Object.values(allFields);
+      if (fieldsArr.length) {
+        const sortedFields = sortFields(fieldsArr);
+        setFields(sortedFields);
+      }
+    }
+  }, [loadedWarehouseFields, allFields]);
 
-                        {/* Fields Display */}
-                        <div className="flex w-3/4 items-center justify-center">
-                            <div className="w-full text-center">
-                                {fields && warehouse ? fieldGenerator(fields, warehouse) : null}
-                            </div>
-                        </div>
-
-                        {/* Right Buttons */}
-                        <div className="rightButtons absolute right-0 top-0 flex flex-col items-center mx-2">
-                            <ModalButton dir="right" operation="add" warehouseId={warehouseId} />
-                            <ModalButton dir="right" operation="subtract" warehouseId={warehouseId} />
-                        </div>
-                    </div>
-
-                    {/* Bottom Buttons */}
-                    <div className="bottomButtons absolute bottom-4 left-1/2 transform -translate-x-1/2 flex items-center gap-4 mt-4">
-                        <ModalButton dir="bottom" operation="add" warehouseId={warehouseId} />
-                        <ModalButton dir="bottom" operation="subtract" warehouseId={warehouseId} />
-                    </div>
-                </div>
-
-                {/* Delete Warehouse Button */}
-                <div className="flex justify-center items-center h-24">
-                    <button
-                        onClick={openDeleteModal}
-                        className="px-5 py-2 bg-red-500 text-white rounded-md cursor-pointer"
-                    >
-                        DELETE
-                    </button>
-
-                    {/* Delete Confirmation Modal */}
-                    {isDeleteModalOpen && (
-                        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10">
-                            <div className="bg-white p-5 rounded-lg shadow-lg text-center">
-                                <p>Are you sure you want to delete the warehouse?</p>
-                                <div className="mt-5">
-                                    <button
-                                        onClick={confirmDelete}
-                                        className="px-5 py-2 bg-red-500 text-white rounded-md mr-2 cursor-pointer"
-                                    >
-                                        Yes
-                                    </button>
-                                    <button
-                                        onClick={closeDeleteModal}
-                                        className="px-5 py-2 bg-gray-300 rounded-md cursor-pointer"
-                                    >
-                                        No
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    )}
-                </div>
+  return (
+    <div>
+      <div className="flex flex-col items-center h-full">
+        <div className="wrapper flex flex-col items-center w-full h-full">
+          <div className="flex w-full mt-4 relative">
+            {/* Left Buttons */}
+            <div className="leftButtons absolute left-0 top-0 flex flex-col items-center mx-2">
+              <ModalButton
+                dir="left"
+                operation="add"
+                warehouseId={warehouseId}
+              />
+              <ModalButton
+                dir="left"
+                operation="subtract"
+                warehouseId={warehouseId}
+              />
             </div>
+
+            {/* Fields Display */}
+            <div className="flex w-3/4 items-center justify-center">
+              <div className="w-full text-center">
+                {fields && warehouse ? fieldGenerator(fields, warehouse) : null}
+              </div>
+            </div>
+
+            {/* Right Buttons */}
+            <div className="rightButtons absolute right-0 top-0 flex flex-col items-center mx-2">
+              <ModalButton
+                dir="right"
+                operation="add"
+                warehouseId={warehouseId}
+              />
+              <ModalButton
+                dir="right"
+                operation="subtract"
+                warehouseId={warehouseId}
+              />
+            </div>
+          </div>
+
+          {/* Bottom Buttons */}
+          <div className="bottomButtons absolute bottom-4 left-1/2 transform -translate-x-1/2 flex items-center gap-4 mt-4">
+            <ModalButton
+              dir="bottom"
+              operation="add"
+              warehouseId={warehouseId}
+            />
+            <ModalButton
+              dir="bottom"
+              operation="subtract"
+              warehouseId={warehouseId}
+            />
+          </div>
         </div>
-    );
+
+        <div className="flex justify-center items-center h-24">
+          <OpenModalButton
+            buttonText="DELETE"
+            onItemClick={openDeleteModal}
+            modalComponent={
+              <DeleteWarehouseModal
+                closeModal={closeDeleteModal}
+                confirmDelete={confirmDelete}
+              />
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
