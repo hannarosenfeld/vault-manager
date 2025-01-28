897ac815d44ae30c387db32d1756abcf04ab6441
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-01-28 09:49:46 -0500

    .

--- react-app/src/components/EditWarehousePage/index.jsx
+++ react-app/src/components/EditWarehousePage/index.jsx
@@ -74,9 +74,9 @@ export default function EditWarehousePage() {
       {/* Top Box (Delete Button) */}
       <div className="wrapper w-full border-2 height-[10vh]">
         <div className="flex justify-center items-center h-24">
-          <button
+        <button
             onClick={openDeleteModal} // Calls the function to open the modal
-            className="btn btn-outline-secondary"
+            className="btn btn-outline-secondary !text-red-700 hover:!text-white !border-red-700 hover:!bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:!border-red-500 dark:!text-red-500 dark:hover:!bg-red-600 dark:focus:ring-red-900"
           >
             DELETE
           </button>
