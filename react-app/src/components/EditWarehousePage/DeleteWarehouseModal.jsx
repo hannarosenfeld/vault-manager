5e7da9b31928f093f10c27cc15f324dc3deaff3b
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-01-28 09:40:36 -0500

    Force tailwind styles.

--- react-app/src/components/EditWarehousePage/DeleteWarehouseModal.jsx
+++ react-app/src/components/EditWarehousePage/DeleteWarehouseModal.jsx
@@ -49,20 +49,21 @@ export default function DeleteWarehouseModal({
             </div>
 
             <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              <button
-                type="button"
-                onClick={confirmDelete}
-                className="btn btn-danger inline-flex w-full justify-center rounded-md bg-red-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-              >
-                Delete
-              </button>
-              <button
-                type="button"
-                onClick={closeDeleteModal}
-                className="btn btn-outline-secondary mt-3 inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-700 shadow-sm ring-1 ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-              >
-                Cancel
-              </button>
+            <button
+              type="button"
+              onClick={confirmDelete}
+              className="!inline-flex !w-full !justify-center !rounded-md !bg-red-600 !px-4 !py-2 !text-sm !font-semibold !text-white !shadow-sm hover:!bg-red-500 sm:!ml-3 sm:!w-auto"
+            >
+              Delete
+            </button>
+
+            <button
+              type="button"
+              onClick={closeDeleteModal}
+              className="!mt-3 !inline-flex !w-full !justify-center !rounded-md !bg-white !px-4 !py-2 !text-sm !font-semibold !text-gray-700 !shadow-sm !ring-1 !ring-gray-300 hover:!bg-gray-50 sm:!mt-0 sm:!w-auto"
+            >
+              Cancel
+            </button>
             </div>
           </DialogPanel>
         </div>
--- react-app/tailwind.config.js
+++ react-app/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   prefix: 'tw-',
+  important: true,
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
