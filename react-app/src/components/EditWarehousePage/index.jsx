07ba1544ffdae970cf331d476ce711e6642a6718
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-01-28 10:50:55 -0500

    Add field toggle.

--- react-app/src/components/EditWarehousePage/index.jsx
+++ react-app/src/components/EditWarehousePage/index.jsx
@@ -71,26 +71,45 @@ export default function EditWarehousePage() {
 
   return (
     <div className="flex flex-col items-center h-[80vh]">
-      {/* Top Box (Delete Button) */}
-      <div className="wrapper w-full border-2 height-[10vh]">
-        <div className="flex justify-center items-center h-24">
-        <button
+
+      
+      <div className="wrapper !w-full !border-2 !h-[10vh] !flex !justify-between !items-center bg-white rounded-lg shadow-md !p-[2em] !mb-[1em]">
+        {/* Toggle Button on the Left */}
+        <div className="!flex !items-center !space-x-3">
+          {/* Toggle Button on the Left */}
+          <label className="inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" className="!sr-only peer" />
+            <div className="!relative w-11 h-6 !bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:!bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:!bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:!bg-blue-600 dark:peer-checked:!bg-blue-600" />
+          </label>
+
+          {/* "Add Fields" Text on the Right */}
+          <span className="!text-sm !font-medium !text-gray-900 !dark:text-gray-300">
+            Add fields
+          </span>
+        </div>
+
+        {/* Delete Button on the Right */}
+        <div>
+          <button
             onClick={openDeleteModal} // Calls the function to open the modal
             className="btn btn-outline-secondary !text-red-700 hover:!text-white !border-red-700 hover:!bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:!border-red-500 dark:!text-red-500 dark:hover:!bg-red-600 dark:focus:ring-red-900"
           >
             DELETE
           </button>
-
-          {isDeleteModalOpen && (
-            <DeleteWarehouseModal
-              isDeleteModalOpen={isDeleteModalOpen}
-              setIsDeleteModalOpen={setIsDeleteModalOpen}
-              confirmDelete={confirmDelete}
-              closeDeleteModal={closeDeleteModal} // Ensure this is passed correctly
-            />
-          )}
         </div>
+
+        {/* Delete Modal */}
+        {isDeleteModalOpen && (
+          <DeleteWarehouseModal
+            isDeleteModalOpen={isDeleteModalOpen}
+            setIsDeleteModalOpen={setIsDeleteModalOpen}
+            confirmDelete={confirmDelete}
+            closeDeleteModal={closeDeleteModal} // Ensure this is passed correctly
+          />
+        )}
       </div>
+
+      
 
       {/* Fields and Buttons Layout */}
       <div className="wrapper flex w-[95%] h-[70vh]">
