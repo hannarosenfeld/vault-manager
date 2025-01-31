b8b84c1fad9ad500dd7c28c610fa83b768d0e7b1
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-01-30 21:37:25 -0500

    Add more detailed wall-sides in rack model.

--- app/models/rack.py
+++ app/models/rack.py
@@ -6,7 +6,7 @@ class Rack(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     shelves = db.Column(db.Integer, nullable=False)
-    wall_side = db.Column(Enum('top', 'bottom', 'left', 'right', name='wall_side_enum'), nullable=False)
+    wall_side = db.Column(Enum('top-left', 'top-right', 'bottom', 'left', 'right', name='wall_side_enum'), nullable=False)
     position = db.Column(Enum('horizontal', 'vertical', name='position_enum'), nullable=False)
 
     warehouse_id = db.Column(db.Integer, db.ForeignKey('warehouses.id'), nullable=False)
