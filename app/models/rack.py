3b3c855900d90711d0cc4084a91fd2073e4699e3
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-01-30 21:06:49 -0500

    Change rack model column facing to position to only include horizontal and vertical.

--- app/models/rack.py
+++ app/models/rack.py
@@ -1,18 +1,20 @@
 from .db import db
-from sqlalchemy import CheckConstraint
+from sqlalchemy import CheckConstraint, Enum
 
 class Rack(db.Model):
     __tablename__ = 'racks'
 
     id = db.Column(db.Integer, primary_key=True)
     shelves = db.Column(db.Integer, nullable=False)
+    wall_side = db.Column(Enum('top', 'bottom', 'left', 'right', name='wall_side_enum'), nullable=False)
+    position = db.Column(Enum('horizontal', 'vertical', name='position_enum'), nullable=False)
 
     warehouse_id = db.Column(db.Integer, db.ForeignKey('warehouses.id'), nullable=False)
 
     # Relationship with RackContent
     contents = db.relationship('RackContent', backref='rack', lazy=True, cascade="all, delete")
 
-    # Constraint to enforce max 10 shelves
+    # Constraints
     __table_args__ = (
         CheckConstraint('shelves BETWEEN 1 AND 10', name='check_shelves_limit'),
     )
@@ -21,6 +23,8 @@ class Rack(db.Model):
         return {
             'id': self.id,
             'shelves': self.shelves,
+            'wall_side': self.wall_side,
+            'position': self.position,
             'warehouse_id': self.warehouse_id,
             'contents': self.contents
         }
