f8121c5d89a3e712f19875db5fd84e104c9ca430
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-02-09 11:42:42 -0500

    Style RackDetail.

--- react-app/src/components/Warehouse/RackInfo/index.jsx
+++ react-app/src/components/Warehouse/RackInfo/index.jsx
@@ -1,24 +1,25 @@
 export const RackInfo = ({ selectedRack }) => {
-    if (!selectedRack?.position) return null;
-  
-    return (
-      <div className="flex">
-        <div className="border-t border-[var(--lightgrey)] w-[75%]">
-          {Array.from({ length: selectedRack.shelves }, (_, index) => (
-            <div
-              key={index}
-              className="flex justify-between items-center p-1 border-b border-[var(--lightgrey)] last:border-b-0"
-            >
-              <span className="">{index + 1}</span>
-            </div>
-          ))}
-        </div>
-        <div className="w-[25%] border-l-1 border-[var(--lightgrey)] flex items-center justify-center">
-          <h2 className="font-light">{selectedRack.position}</h2>
-        </div>
+  if (!selectedRack?.position) return null;
+
+  return (
+    <div className="flex">
+      <div className="w-[75%]">
+        {Array.from({ length: selectedRack.shelves }, (_, index) => (
+          <div
+            key={index}
+            className={`flex justify-between items-center p-1 border-[var(--lightgrey)] 
+              ${index === 0 ? "" : "border-t"} 
+              ${index === selectedRack.shelves - 1 ? "" : "border-b"}`}
+          >
+            <span>{index + 1}</span>
+          </div>
+        ))}
       </div>
-    );
-  };
-  
-  export default RackInfo;
-  
+      <div className="w-[25%] border-l border-[var(--lightgrey)] flex items-center justify-center">
+        <h2 className="font-light">{selectedRack.position}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default RackInfo;
