c785cbc0346fcd1fa870058a12413f0667a0ca57
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-02-11 22:38:06 -0500

    Add more warehouse seeders.

--- app/seeds/warehouse.py
+++ app/seeds/warehouse.py
@@ -1,27 +1,34 @@
 from sqlalchemy.sql import text
-from app.models import db, Warehouse, Field, Vault,Order, environment, SCHEMA, User  # Import User model
+from app.models import db, Warehouse, Field, Vault, Order, environment, SCHEMA, User  # Import User model
 
 def seed_warehouse(users, fields, orders):
     allFields = Field.query.all()
     user_instances = User.query.all()
     order_instances = Order.query.all()
 
-    print("👰🏼‍♀️", allFields, user_instances,order_instances)
-    
-    warehouse = Warehouse()
-    warehouse.name = "Warehouse 3"
-    warehouse.cols = 9
-    warehouse.rows = 12
-    warehouse.warehouse_fields = allFields
-    warehouse.users = user_instances
-    warehouse.orders = order_instances
+    warehouses_data = [
+        {"name": "Warehouse 1", "cols": 9, "rows": 12},
+        {"name": "Warehouse 2", "cols": 9, "rows": 12},
+        {"name": "Warehouse 3", "cols": 9, "rows": 12},
+        {"name": "Warehouse 4", "cols": 9, "rows": 12},
+    ]
 
-    print("🦋", warehouse)
+    warehouses = []
+    for warehouse_data in warehouses_data:
+        warehouse = Warehouse()
+        warehouse.name = warehouse_data["name"]
+        warehouse.cols = warehouse_data["cols"]
+        warehouse.rows = warehouse_data["rows"]
+        warehouse.warehouse_fields = allFields
+        warehouse.users = user_instances
+        warehouse.orders = order_instances
 
-    db.session.add(warehouse)
+        db.session.add(warehouse)
+        warehouses.append(warehouse)
+
     db.session.commit()
 
-    return [warehouse]
+    return warehouses
 
 def undo_warehouse():
     if environment == "production":
