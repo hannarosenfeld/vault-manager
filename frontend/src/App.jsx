2eadc4149efdf6bfaaab2adde5063e8f76fae850
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-02-11 22:54:39 -0500

    Universal background.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="flex flex-col m-auto w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-[800px]">
+    <div className="flex flex-col  bg-gray-200 m-auto w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-[800px]">
       <NavBar />
-      <div className="h-[90vh] bg-gray-200">
+      <div className="h-[90vh]">
         <HomePage />
       </div>
     </div>
