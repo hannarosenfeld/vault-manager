ba51206846257e72a13526b8c5510414b2ac350b
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-02-12 15:13:04 -0500

    Get field info.

--- frontend/src/components/Warehouse/FieldInfo.jsx
+++ frontend/src/components/Warehouse/FieldInfo.jsx
@@ -1,5 +1,12 @@
 export default function FieldInfo({ field }) {
-  return(
-    <div>{field.name}</div>
-  )
+  return (
+    <div className="h-[90%] grid grid-cols-[75%_25%]">
+      <div className="grid grid-rows-3 border-r-1 border-gray-300">
+        <div className="border-b-1 border-gray-300 p-2">hi</div>
+        <div className="border-b-1 border-gray-300 p-2">hi</div>
+        <div className="p-2">hi</div>
+      </div>
+      <div className="flex items-center justify-center p-2">{field.name}</div>
+    </div>
+  );
 }
