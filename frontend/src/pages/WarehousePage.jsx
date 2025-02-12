6c42847567b081bd616d8516372fd05182103312
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-02-12 08:17:22 -0500

    Warehouse detail page layout.

--- frontend/src/pages/WarehousePage.jsx
+++ frontend/src/pages/WarehousePage.jsx
@@ -1,7 +1,26 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 function WarehousePage() {
+  const { warehouseName } = useParams();
+  const warehouse = useSelector((state) =>
+    Object.values(state.warehouse.warehouses).find(
+      (w) => w.name.toLowerCase().split(" ").join("-") === warehouseName
+    )
+  );
+
+  if (!warehouse) {
+    return <div>Warehouse not found</div>;
+  }
+
   return (
-    <div className="">
-      <h1>Warehouse Details</h1>
+    <div className="h-full">
+      <h1 className="text-xl font-bold mb-2 text-center">{warehouse.name}</h1>
+      <div className="border-2 h-[23vh]">
+      </div>
+      <div className="border-2 min-h-[60vh]">
+
+      </div>
     </div>
   );
 }
