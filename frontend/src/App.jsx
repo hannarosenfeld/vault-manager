fcf7eddc4fabb914798332a3969a6526ce336f41
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-02-11 23:18:07 -0500

    Change style again.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="flex flex-col bg-gray-50 m-auto w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-[800px]">
+    <div className="flex flex-col bg-gray-100 m-auto w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-[800px]">
       <NavBar />
       <div className="h-[90vh]">
         <HomePage />
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -23,8 +23,8 @@ export default function HomePage() {
           {/* Render your warehouses here */}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {Object.values(warehouses).map((warehouse) => (
-              <div key={warehouse.id} className="border border-gray-300 p-4 rounded-lg">
-                <h2 className="text-lg font-semibold text-gray-700">{warehouse.name}</h2>
+              <div key={warehouse.id} className="border border-gray-300 p-4 rounded-lg bg-white">
+                <h2 className="text-lg font-semibold text-gray-800">{warehouse.name}</h2>
                 <p>{warehouse.location}</p>
               </div>
             ))}
