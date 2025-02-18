6637d11d37402c83b65532ce63b4a32083aa7449
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-02-18 12:10:07 -0500

    Show vault name in edit vault modal.

--- frontend/src/components/Warehouse/ViewAndEditVaultModal.jsx
+++ frontend/src/components/Warehouse/ViewAndEditVaultModal.jsx
@@ -73,12 +73,12 @@ export default function ViewAndEditVaultModal({ toggleModal, vault }) {
             <input
               type="text"
               name="vault_id"
-              value={editableVault.vault_id}
+              value={editableVault.name}
               onChange={handleChange}
               className="ml-2 mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
             />
           ) : (
-            <span className="ml-2 w-2/3">{editableVault.vault_id}</span>
+            <span className="ml-2 w-2/3">{editableVault.name}</span>
           )}
           <svg
             xmlns="http://www.w3.org/2000/svg"
