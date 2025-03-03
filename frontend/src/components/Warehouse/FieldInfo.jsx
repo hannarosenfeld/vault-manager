44431225ee1f943a82e12912cbf7323d51a622a5
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-03-03 10:04:19 -0500

    Set field to vault field by default.

--- frontend/src/components/Warehouse/FieldInfo.jsx
+++ frontend/src/components/Warehouse/FieldInfo.jsx
@@ -12,7 +12,7 @@ export default function FieldInfo({ field, isStage, vaultId, onMove }) {
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
   const [selectedPosition, setSelectedPosition] = useState(null);
   const [vaults, setVaults] = useState(field.vaults);
-  const [fieldType, setFieldType] = useState(field.type);
+  const [fieldType, setFieldType] = useState("standard"); // Set initial state to "standard"
 
   const rowCount = fieldType === "couchbox" ? 4 : 3;
 
