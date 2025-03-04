1a905eea09993e4f5f855f4928fd552084c113f7
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-03-04 15:43:04 -0500

    Add Warehouse in nav drawer.

--- frontend/src/components/NavBar/NavDrawer.jsx
+++ frontend/src/components/NavBar/NavDrawer.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../../store/session"; // Import the logout action
 
-export default function NavDrawer({ open, setOpen }) {
+export default function NavDrawer({ open, setOpen, onAddWarehouse }) {
   const dispatch = useDispatch();
   const warehouses = useSelector((state) => state.warehouse.warehouses);
   const warehouseArr = Object.values(warehouses);
@@ -74,6 +74,27 @@ export default function NavDrawer({ open, setOpen }) {
                     <span className="material-symbols-outlined">package_2</span>
                     <span className="ml-3">Stage</span>
                   </Link>
+                  {/* Add Warehouse Button */}
+                  <button
+                    onClick={onAddWarehouse}
+                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group w-full"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 4.5v15m7.5-7.5h-15"
+                      />
+                    </svg>
+                    <span className="ml-3">Add Warehouse</span>
+                  </button>
                   {/* Logout Button */}
                   <button
                     onClick={handleLogout}
