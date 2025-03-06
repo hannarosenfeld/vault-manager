ca6fe3fe3e4951224973cf0a558920244fdba48d
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-03-06 08:59:52 -0500

    EditWarehouseModal.

--- frontend/src/components/EditWarehouse/EditWarehouseModal.jsx
+++ frontend/src/components/EditWarehouse/EditWarehouseModal.jsx
@@ -37,7 +37,8 @@ export function EditWarehouseModal({ dir, operation, warehouseId, onClose }) {
             <p className="text-gray-600">
               {operation === "plus" ? "Add" : "Subtract"}{" "}
               <span className="font-bold">{count}</span> row
-              {count === 1 ? "" : "s"} {operation === "add" ? "to" : "from"} warehouse <b>{dir}</b>
+              {count === 1 ? "" : "s"} {operation === "plus" ? "to" : "from"}{" "}
+              warehouse <b>{dir}</b>
             </p>
           </div>
           <div className="flex items-center justify-center mb-4">
@@ -64,17 +65,16 @@ export function EditWarehouseModal({ dir, operation, warehouseId, onClose }) {
               </button>
             </div>
           </div>
-          <div className="flex justify-end">
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
-            >
-              Submit
+          <div className="flex justify-end mt-10">
+          <button
+            type="submit"
+            className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+          >Submit
             </button>
             <button
               type="button"
+              className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
               onClick={onClose}
-              className="bg-gray-300 text-gray-700 px-4 py-2 rounded"
             >
               Cancel
             </button>
