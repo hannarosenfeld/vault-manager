5f8361a6725ba218e6876d225a70e6a433b36d2f
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-03-06 15:43:18 -0500

    Style edit warehouse.

--- frontend/src/components/EditWarehouse/ActionButton.jsx
+++ frontend/src/components/EditWarehouse/ActionButton.jsx
@@ -1,7 +1,11 @@
 const ActionButton = ({ onClick, icon, label }) => (
-  <button className="btn btn-outline-secondary mb-2" onClick={onClick}>
-    <span className="material-symbols-outlined">{icon}</span>
-    {label && <span className="ml-2">{label}</span>}
+  <button
+    type="button"
+    onClick={onClick}
+    className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-2 focus:ring-gray-100 font-medium rounded-md text-xs px-1 py-0.5 me-0.5 mb-1"
+  >
+    <span className="material-symbols-outlined text-xs">{icon}</span>
+    {label && <span className="ml-0.5 text-xs">{label}</span>}
   </button>
 );
 
--- frontend/src/pages/EditWarehousePage.jsx
+++ frontend/src/pages/EditWarehousePage.jsx
@@ -7,7 +7,6 @@ import LoadingSpinner from "../components/LoadingSpinner";
 import ActionButton from "../components/EditWarehouse/ActionButton";
 import EditWarehouseModal from "../components/EditWarehouse/EditWarehouseModal";
 
-
 export default function EditWarehousePage() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -20,7 +19,9 @@ export default function EditWarehousePage() {
 
   useEffect(() => {
     const foundWarehouse = Object.values(warehouses).find(
-      (w) => w.name.toLowerCase().split(" ").join("-") === warehouseName.toLowerCase()
+      (w) =>
+        w.name.toLowerCase().split(" ").join("-") ===
+        warehouseName.toLowerCase()
     );
     if (foundWarehouse) {
       dispatch(setCurrentWarehouse(foundWarehouse));
@@ -40,30 +41,67 @@ export default function EditWarehousePage() {
   };
 
   return (
-    <div className="flex flex-col items-center h-full mt-5">
+    <div className="flex flex-col items-center h-full mt-6">
       <h2 className="mb-1 text-xl font-semibold">{warehouse.name}</h2>
 
       <div className="flex w-full mt-4 justify-center">
         <div className="flex flex-col items-center mx-4 justify-center">
-          <ActionButton onClick={() => openModal('left', 'plus')} icon="add" />
-          <ActionButton onClick={() => openModal('left', 'minus')} icon="remove" />
+          <ActionButton onClick={() => openModal("left", "plus")} icon="add" />
+          <ActionButton
+            onClick={() => openModal("left", "minus")}
+            icon="remove"
+          />
         </div>
         <div className="flex-grow flex justify-center">
           <EditWarehouseFieldGrid warehouse={warehouse} />
         </div>
         <div className="flex flex-col items-center mx-4 justify-center">
-          <ActionButton onClick={() => openModal('right', 'plus')} icon="add" />
-          <ActionButton onClick={() => openModal('right', 'minus')} icon="remove" />
+          <ActionButton onClick={() => openModal("right", "plus")} icon="add" />
+          <ActionButton
+            onClick={() => openModal("right", "minus")}
+            icon="remove"
+          />
         </div>
       </div>
-      <div className="flex items-center gap-4 mt-4 justify-center">
-        <ActionButton onClick={() => openModal('bottom', 'plus')} icon="add" />
-        <ActionButton onClick={() => openModal('bottom', 'minus')} icon="remove" />
+      <div className="flex items-center gap-1 mt-4 justify-center">
+        <ActionButton onClick={() => openModal("bottom", "plus")} icon="add" />
+        <ActionButton
+          onClick={() => openModal("bottom", "minus")}
+          icon="remove"
+        />
       </div>
 
       {isModalOpen && (
-        <EditWarehouseModal {...modalProps} onClose={() => setIsModalOpen(false)} />
+        <EditWarehouseModal
+          {...modalProps}
+          onClose={() => setIsModalOpen(false)}
+        />
       )}
+
+<hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+
+
+
+      <button
+        type="button"
+        class="flex mt-6 gap-1 align-center items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+          />
+        </svg>
+        Delete Warehouse
+      </button>
     </div>
   );
 }
