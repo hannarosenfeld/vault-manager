707595bd967c107e43181dbe220bbb409f020515
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-03-06 20:39:11 -0500

    Fix typo.

--- frontend/src/pages/EditWarehousePage.jsx
+++ frontend/src/pages/EditWarehousePage.jsx
@@ -82,7 +82,7 @@ export default function EditWarehousePage() {
 
       <button
         type="button"
-        class="flex mt-3 gap-1 align-center items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+        className="flex mt-3 gap-1 align-center items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
