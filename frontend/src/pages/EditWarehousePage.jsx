2fc01e7e8867537920688187af41a48523a2ce20
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-03-06 20:37:22 -0500

    Style editwarehouse page.

--- frontend/src/pages/EditWarehousePage.jsx
+++ frontend/src/pages/EditWarehousePage.jsx
@@ -78,13 +78,11 @@ export default function EditWarehousePage() {
         />
       )}
 
-<hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-
-
+      <hr class="w-full h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
 
       <button
         type="button"
-        class="flex mt-6 gap-1 align-center items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+        class="flex mt-3 gap-1 align-center items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
