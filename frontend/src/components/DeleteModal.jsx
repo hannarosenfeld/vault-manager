f7d90eb025f2a2596baad5d096dafe1724b3bcb6
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-03-10 14:54:44 -0400

    Center delete warehouse modal.

--- frontend/src/components/DeleteModal.jsx
+++ frontend/src/components/DeleteModal.jsx
@@ -1,9 +1,7 @@
-
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 
-
-export default function DeleteModal({ onClose, warehouse }) {  
+export default function DeleteModal({ onClose, warehouse }) {
   return (
     <Dialog open={true} onClose={onClose} className="relative z-10">
       <DialogBackdrop
@@ -11,8 +9,8 @@ export default function DeleteModal({ onClose, warehouse }) {
         className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
       />
 
-      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+      <div className="fixed inset-0 z-10 flex items-center justify-center overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
           <DialogPanel
             transition
             className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
@@ -28,7 +26,7 @@ export default function DeleteModal({ onClose, warehouse }) {
                   </DialogTitle>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
-                      Are you sure you want to delete <span className='font-semibold'>{warehouse.name}</span>? All of it's data will be permanently removed.
+                      Are you sure you want to delete <span className='font-semibold'>{warehouse.name}</span>? All of its data will be permanently removed.
                       This action cannot be undone.
                     </p>
                   </div>
