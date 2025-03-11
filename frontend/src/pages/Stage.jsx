71181b907f19feba341f76387e679ed8c31e96b2
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-03-11 15:56:26 -0400

    Add padding to stage.

--- frontend/src/pages/Stage.jsx
+++ frontend/src/pages/Stage.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import StageToWareHouseModal from '../components/Stage/StageToWarehouseModal';
-import ConfirmDeleteVaultModal from '../components/Stage/ConfirmDeleteVaultModal';
-import { deleteVaultThunk } from '../store/warehouse';
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import StageToWareHouseModal from "../components/Stage/StageToWarehouseModal";
+import ConfirmDeleteVaultModal from "../components/Stage/ConfirmDeleteVaultModal";
+import { deleteVaultThunk } from "../store/warehouse";
 
 export default function Stage() {
   const dispatch = useDispatch();
@@ -37,7 +37,9 @@ export default function Stage() {
 
   const handleConfirmDelete = async () => {
     try {
-      await Promise.all(vaultsToDelete.map((vault) => dispatch(deleteVaultThunk(vault.id))));
+      await Promise.all(
+        vaultsToDelete.map((vault) => dispatch(deleteVaultThunk(vault.id)))
+      );
       setVaultsToDelete([]);
       setIsDeleteModalOpen(false);
       console.log("Vaults deleted!");
@@ -66,69 +68,94 @@ export default function Stage() {
   };
 
   return (
-    <div className="min-h-[90vh] max-w-3xl mx-auto border-8 border-solid p-5"
-      style={{
-        borderImage: isDeleteModeOn
-          ? "repeating-linear-gradient(-55deg, #000, #000 20px, #ff0000 20px, #ff0000 40px) 10"
-          : "repeating-linear-gradient(-55deg, #000, #000 20px, #ffb101 20px, #ffb101 40px) 10",
-      }}
-    >
-      <div className="flex justify-between items-center mb-4 h-12">
-        <div className="flex items-center">
-          <label className="flex items-center cursor-pointer">
-            <div className="relative">
-              <input
-                type="checkbox"
-                checked={isDeleteModeOn}
-                onChange={handleToggleDeleteMode}
-                className="sr-only"
-              />
-              <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
-              <div
-                className={`dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition ${
-                  isDeleteModeOn ? "transform translate-x-full bg-red-500" : ""
-                }`}
-              ></div>
-            </div>
-            <span className="ml-3 text-gray-700">Delete vaults mode {isDeleteModeOn ? <span className='text-green-600'>ON</span> : <span className='text-red-600'>OFF</span>}</span>
-          </label>
-        </div>
-        {isDeleteModeOn && (
-          <button
-            onClick={handleDeleteClick}
-            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-          >
-            Delete Selected
-          </button>
-        )}
-      </div>
-      {Object.keys(stagedVaults).length === 0 ? (
-        <p className="text-gray-500">No staged vaults available.</p>
-      ) : (
-        <div className="flex flex-wrap gap-4">
-          {Object.values(stagedVaults).map((vault) => (
-            <div
-              key={vault.id}
-              className={`flex flex-col justify-center items-center p-2 border-2 border-gray-800 rounded shadow cursor-pointer hover:bg-gray-100 w-[calc(33.333%-1rem)] ${
-                isDeleteModeOn && vaultsToDelete.some((v) => v.id === vault.id) ? "bg-red-500" : "bg-yellow-400"
-              }`}
-              onClick={() => handleVaultClick(vault)}
+    <div className="p-4">
+      <div
+        className="min-h-[90vh] max-w-3xl mx-auto border-8 border-solid p-5"
+        style={{
+          borderImage: isDeleteModeOn
+            ? "repeating-linear-gradient(-55deg, #000, #000 20px, #ff0000 20px, #ff0000 40px) 10"
+            : "repeating-linear-gradient(-55deg, #000, #000 20px, #ffb101 20px, #ffb101 40px) 10",
+        }}
+      >
+        <div className="flex justify-between items-center mb-4 h-12">
+          <div className="flex items-center">
+            <label className="flex items-center cursor-pointer">
+              <div className="relative">
+                <input
+                  type="checkbox"
+                  checked={isDeleteModeOn}
+                  onChange={handleToggleDeleteMode}
+                  className="sr-only"
+                />
+                <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
+                <div
+                  className={`dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition ${
+                    isDeleteModeOn
+                      ? "transform translate-x-full bg-red-500"
+                      : ""
+                  }`}
+                ></div>
+              </div>
+              <span className="ml-3 text-gray-700">
+                Delete vaults mode{" "}
+                {isDeleteModeOn ? (
+                  <span className="text-green-600">ON</span>
+                ) : (
+                  <span className="text-red-600">OFF</span>
+                )}
+              </span>
+            </label>
+          </div>
+          {isDeleteModeOn && (
+            <button
+              onClick={handleDeleteClick}
+              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
             >
-              <h2 className="text-xs text-blue-500 font-semibold">#{vault.id}</h2>
-              <p className="text-xs font-semibold text-gray-700 text-center">{vault.customer_name}</p>
-              {vault.order_name && (
-                <p className="text-xs text-gray-700 text-center">[{vault.order_name}]</p>
-              )}
-            </div>
-          ))}
+              Delete Selected
+            </button>
+          )}
         </div>
-      )}
-      <StageToWareHouseModal isOpen={isModalOpen} onClose={closeModal} vault={selectedVault} />
-      <ConfirmDeleteVaultModal
-        isOpen={isDeleteModalOpen}
-        onClose={handleCloseDeleteModal}
-        handleDelete={handleConfirmDelete}
-      />
+        {Object.keys(stagedVaults).length === 0 ? (
+          <p className="text-gray-500">No staged vaults available.</p>
+        ) : (
+          <div className="flex flex-wrap gap-4">
+            {Object.values(stagedVaults).map((vault) => (
+              <div
+                key={vault.id}
+                className={`flex flex-col justify-center items-center p-2 border-2 border-gray-800 rounded shadow cursor-pointer hover:bg-gray-100 w-[calc(33.333%-1rem)] ${
+                  isDeleteModeOn &&
+                  vaultsToDelete.some((v) => v.id === vault.id)
+                    ? "bg-red-500"
+                    : "bg-yellow-400"
+                }`}
+                onClick={() => handleVaultClick(vault)}
+              >
+                <h2 className="text-xs text-blue-500 font-semibold">
+                  #{vault.id}
+                </h2>
+                <p className="text-xs font-semibold text-gray-700 text-center">
+                  {vault.customer_name}
+                </p>
+                {vault.order_name && (
+                  <p className="text-xs text-gray-700 text-center">
+                    [{vault.order_name}]
+                  </p>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+        <StageToWareHouseModal
+          isOpen={isModalOpen}
+          onClose={closeModal}
+          vault={selectedVault}
+        />
+        <ConfirmDeleteVaultModal
+          isOpen={isDeleteModalOpen}
+          onClose={handleCloseDeleteModal}
+          handleDelete={handleConfirmDelete}
+        />
+      </div>
     </div>
   );
 }
