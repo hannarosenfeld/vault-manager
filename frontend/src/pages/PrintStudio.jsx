e72c6a7cd9e76b53c2702dd91dea5e907ec94daa
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-03-13 13:13:03 -0400

    Responsive style.

--- frontend/src/pages/PrintStudio.jsx
+++ frontend/src/pages/PrintStudio.jsx
@@ -34,7 +34,9 @@ function PrintStudio() {
   };
 
   // Sort vaults by customer name
-  const sortedVaults = [...vaults].sort((a, b) => a.customer_name.localeCompare(b.customer_name));
+  const sortedVaults = [...vaults].sort((a, b) =>
+    a.customer_name.localeCompare(b.customer_name)
+  );
 
   return (
     <div className="p-4">
@@ -42,12 +44,13 @@ function PrintStudio() {
       <div className="mt-4">
         <button
           onClick={handleDownloadCSV}
-          className="p-2.5 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+          className="flex items-center p-2.5 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
         >
+          <span className="material-symbols-outlined mr-2">download</span>
           Download CSV
         </button>
       </div>
-      <div className="mt-4">
+      <div className="mt-4 overflow-x-auto">
         {sortedVaults.length > 0 ? (
           <table className="min-w-full bg-white">
             <thead>
@@ -64,11 +67,19 @@ function PrintStudio() {
               {sortedVaults.map((vault) => (
                 <tr key={vault.id} className="text-xs">
                   <td className="border px-4 py-2 text-center">{vault.name}</td>
-                  <td className="border px-4 py-2 text-center">{vault.customer_name}</td>
+                  <td className="border px-4 py-2 text-center">
+                    {vault.customer_name}
+                  </td>
                   <td className="border px-4 py-2 text-center">{vault.type}</td>
-                  <td className="border px-4 py-2 text-center">{vault.position}</td>
-                  <td className="border px-4 py-2 text-center">{vault.field_name}</td>
-                  <td className="border px-4 py-2 text-center">{vault.warehouse_name}</td>
+                  <td className="border px-4 py-2 text-center">
+                    {vault.position}
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+                    {vault.field_name}
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+                    {vault.warehouse_name}
+                  </td>
                 </tr>
               ))}
             </tbody>
