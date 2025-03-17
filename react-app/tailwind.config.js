001aab6b9c393e3e5a49da1f595ffa61278f20ff
hannarosenfeld <hannazrosenfeld@gmail.com>
2025-03-17 15:52:47 -0400

    Cleanup.

--- react-app/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  prefix: "tw-",
-  important: true,
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}", "./src/*.{js,jsx,ts,tsx}"],
-  plugins: [require("daisyui")],
-};
